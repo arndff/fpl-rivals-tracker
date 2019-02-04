4fbcaf05f4e143a925398c7a2e4a8001b415b3a5
arndff_ <jrarnaudov@gmail.com>
2019-02-04 13:49:15 +0200

    Adding user input validation

--- hth_main.py
+++ hth_main.py
@@ -2,12 +2,19 @@ from analyzers.HthAnalyzer import HthAnalyzer
 
 
 def execute():
-    team_id = int(input("Enter your team ID: "))
-    print("You're going to see your different players in each H2H match this GW. It'll take a few seconds...\n")
-    hth_analyzer = HthAnalyzer(team_id)
-    hth_analyzer.print_all_matchups()
+    while True:
+        try:
+            team_id = int(input("Enter your team ID: "))
+        except ValueError:
+            print("Please enter a valid integer! Try again.\n")
+            continue
 
-    print("Good luck, {}! :)".format(hth_analyzer.manager_name))
+        print("You're going to see your different players in each H2H match this GW. It'll take a few seconds...\n")
+        hth_analyzer = HthAnalyzer(team_id)
+        hth_analyzer.print_all_matchups()
+
+        print("Good luck, {}! :)".format(hth_analyzer.manager_name))
+        break
 
 
 def main():

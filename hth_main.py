d00b522a4154e84facf3033f25b8fbebcd03b563
arndff <jrarnaudov@gmail.com>
2019-08-24 22:36:41 +0300

    Refactor both main files

--- hth_main.py
+++ hth_main.py
@@ -9,11 +9,13 @@ from fileutils.FileUtils import FileUtils
 """
 
 
-def execute():
+def validate_args():
     if len(sys.argv) > 2:
         print("The script must be called with 0 or 1 argument.")
         sys.exit(1)
 
+
+def read_input():
     team_id = -1
 
     while team_id == -1:
@@ -22,6 +24,14 @@ def execute():
             print()
         except ValueError:
             print("Please enter a valid integer! Try again.\n")
+
+    return team_id
+
+
+def execute():
+    validate_args()
+
+    team_id = read_input()
 
     if len(sys.argv) == 1:
         hth_analyzer = HthAnalyzer(team_id)
--- main.py
+++ main.py
@@ -11,17 +11,17 @@ from fileutils.FileUtils import FileUtils
 """
 
 
-def execute():
-    if len(sys.argv) != 2:
+def validate_args():
+    if len(sys.argv) == 1:
         print("File path argument is missing.")
         sys.exit(1)
 
-    if not FileUtils.validate_input(sys.argv[1]):
+    if len(sys.argv) > 2:
+        print("You must provide exactly one argument.")
         sys.exit(1)
 
-    analyzer = ClassicAnalyzer(sys.argv[1])
-    analyzer.print_table()
 
+def load_stats_menu(analyzer):
     user_input = input("\nStats menu is about to get loaded. Do you want to proceed? [Y/n] ")
 
     if user_input == "Y" or user_input == "y":
@@ -29,6 +29,18 @@ def execute():
     else:
         print("Abort.")
         sys.exit(0)
+
+
+def execute():
+    validate_args()
+
+    if not FileUtils.validate_input(sys.argv[1]):
+        sys.exit(1)
+
+    analyzer = ClassicAnalyzer(sys.argv[1])
+    analyzer.print_table()
+
+    load_stats_menu(analyzer)
 
 
 def main():
